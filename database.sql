e6813dbb6dcea6fb65c9b304c0f06bd2a8ec779b
khannedy <echo.khannedy@gmail.com>
2024-11-15 19:37:12 +0700

    Add initial database schema
    This commit introduces the initial SQL schema for the project. It includes the creation of two tables: 'category' and 'brands' with their respective fields and constraints. Basic select queries for each table are also added for quick verification.

---
+++ database.sql
@@ -0,0 +1,20 @@
+create table category
+(
+    id          varchar(100) primary key,
+    name        varchar(100) not null,
+    description text
+);
+
+select *
+from category;
+
+create table brands
+(
+    id          varchar(100) primary key,
+    name        varchar(100) not null,
+    description text,
+    created_at  timestamp    not null default current_timestamp,
+    updated_at  timestamp    not null default current_timestamp
+);
+
+select * from brands;
