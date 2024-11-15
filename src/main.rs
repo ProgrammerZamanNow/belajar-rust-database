49a32306a8e4568021b83f4678a04a43f06a1c1e
khannedy <echo.khannedy@gmail.com>
2024-11-15 18:04:44 +0700

    Add test for manual PostgreSQL connection
    This commit introduces a test module with a single async test to verify manual connection to a PostgreSQL database using the sqlx library. The test ensures the database connection can be established and then closed correctly.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
 fn main() {
     println!("Hello, world!");
 }
+
+#[cfg(test)]
+mod tests {
+    use sqlx::{Connection, Error, PgConnection};
+
+    #[tokio::test]
+    async fn test_manual_connection() -> Result<(), Error> {
+        let url = "postgres://khannedy:@localhost:5432/belajar_rust_database";
+        let connection: PgConnection = PgConnection::connect(url).await?;
+
+        connection.close().await?;
+        Ok(())
+    }
+
+}
