46b1061870c8ac3d0f1c38499894685fabae5ccd
khannedy <echo.khannedy@gmail.com>
2024-11-15 18:13:19 +0700

    Add connection pool tests for Postgres
    This commit introduces a new test, `test_pool_connection`, that verifies establishing and closing a pooled connection to a PostgreSQL database using sqlx. It also includes the implementation of a `get_pool` function to create a connection pool with specified configurations.

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,26 @@ fn main() {
 
 #[cfg(test)]
 mod tests {
-    use sqlx::{Connection, Error, PgConnection};
+    use std::time::Duration;
+    use sqlx::{Connection, Error, PgConnection, Pool, Postgres};
+    use sqlx::postgres::PgPoolOptions;
+
+    #[tokio::test]
+    async fn test_pool_connection() -> Result<(), Error> {
+        let pool = get_pool().await?;
+        pool.close().await;
+        Ok(())
+    }
+
+    async fn get_pool() -> Result<Pool<Postgres>, Error> {
+        let url = "postgres://khannedy:@localhost:5432/belajar_rust_database";
+        PgPoolOptions::new()
+            .max_connections(10)
+            .min_connections(5)
+            .acquire_timeout(Duration::from_secs(5))
+            .idle_timeout(Duration::from_secs(60))
+            .connect(url).await
+    }
 
     #[tokio::test]
     async fn test_manual_connection() -> Result<(), Error> {
@@ -14,5 +33,4 @@ mod tests {
         connection.close().await?;
         Ok(())
     }
-
 }
