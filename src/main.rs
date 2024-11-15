29db9019888c983a92f5914b2617be116311e3e6
khannedy <echo.khannedy@gmail.com>
2024-11-15 18:41:15 +0700

    Add futures crate and SQLX query test functions
    Added the futures crate as a dependency in Cargo.toml. Implemented multiple SQLX query test functions in main.rs to fetch, fetch_all, fetch_one, and fetch_optional from the category table.

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,80 @@ fn main() {
 #[cfg(test)]
 mod tests {
     use std::time::Duration;
-    use sqlx::{Connection, Error, PgConnection, Pool, Postgres};
-    use sqlx::postgres::PgPoolOptions;
+    use futures::TryStreamExt;
+    use sqlx::{Connection, Error, PgConnection, Pool, Postgres, Row};
+    use sqlx::postgres::{PgPoolOptions, PgRow};
+
+    #[tokio::test]
+    async fn test_fetch() -> Result<(), Error> {
+        let pool = get_pool().await?;
+
+        let mut result = sqlx::query("select * from category")
+            .fetch(&pool);
+
+        while let Some(row) = result.try_next().await? {
+            let id: String = row.get("id");
+            let name: String = row.get("name");
+            let description: String = row.get("description");
+            println!("id : {}, name : {}, description : {}", id, name, description);
+        }
+
+        Ok(())
+    }
+
+    #[tokio::test]
+    async fn test_fetch_all() -> Result<(), Error> {
+        let pool = get_pool().await?;
+
+        let result: Vec<PgRow> = sqlx::query("select * from category")
+            .fetch_all(&pool).await?;
+
+        for row in result {
+            let id: String = row.get("id");
+            let name: String = row.get("name");
+            let description: String = row.get("description");
+            println!("id : {}, name : {}, description : {}", id, name, description);
+        }
+
+        Ok(())
+    }
+
+    #[tokio::test]
+    async fn test_fetch_one() -> Result<(), Error> {
+        let pool = get_pool().await?;
+
+        let result: PgRow = sqlx::query("select * from category where id = $1")
+            .bind("A")
+            .fetch_one(&pool).await?;
+
+        let id: String = result.get("id");
+        let name: String = result.get("name");
+        let description: String = result.get("description");
+        println!("id : {}, name : {}, description : {}", id, name, description);
+
+        Ok(())
+    }
+
+    #[tokio::test]
+    async fn test_fetch_optional() -> Result<(), Error> {
+        let pool = get_pool().await?;
+
+        let result: Option<PgRow> = sqlx::query("select * from category where id = $1")
+            .bind("A")
+            .fetch_optional(&pool).await?;
+
+        if let Some(row) = result {
+            let id: String = row.get("id");
+            let name: String = row.get("name");
+            let description: String = row.get("description");
+            println!("id : {}, name : {}, description : {}", id, name, description)
+        } else {
+            println!("Data is not found")
+        }
+
+        Ok(())
+    }
+
 
     #[tokio::test]
     async fn test_prepare_statement() -> Result<(), Error> {
